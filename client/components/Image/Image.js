655f4eaf32b142bd2788cf0cfa0f0a1329050e14
Shi Ning Le <shining@ualberta.ca>
2023-01-22 11:27:31 -0800

    Update Image.js

--- client/components/Image/Image.js
+++ client/components/Image/Image.js
@@ -1,24 +1,30 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import styles from "./Image.module.css";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from './Image.module.css';
 
 const Image = ({ queryParam }) => {
-	const [result, setResult] = useState("");
+    const [result, setResult] = useState('');
 
-	// useEffect(() => {
-	// 	async function fetchData() {
-	// 		const result = await axios.get(
-	// 			`https://www.googleapis.com/customsearch/v1?key=AIzaSyA29ueKkgh1GRrXMXsTvDnQoyOfo6_10kE&cx=e0539b2f27bb94abf&q=${queryParam}+food&searchType=image&start=1&num=1`
-	// 		);
+    useEffect(() => {
+        async function fetchData() {
+            const result = await axios.get(
+                `https://www.googleapis.com/customsearch/v1?key=AIzaSyAnRA9N5OBTAKqb8sScaegM-YPyriTpByQ&cx=655d6cda3a6114be9&q=${queryParam}+food&searchType=image&start=1&num=1`
+            );
 
-	// 		setResult(result);
-	// 	}
-	// 	fetchData();
-	// }, []);
+            setResult(result);
+        }
+        fetchData();
+    }, []);
 
-	// return <img className={styles.image} src={result && result.data.items[0].link} alt={queryParam} />
+    return (
+        <img
+            className={styles.image}
+            src={result && result.data.items[0].link}
+            alt={queryParam}
+        />
+    );
 
-	return <img className={styles.image} src="favicon.ico" alt="Placholder image -- need new Google API key" />
+    // return <img className={styles.image} src="favicon.ico" alt="Placholder image -- need new Google API key" />
 };
 
 export default Image;
