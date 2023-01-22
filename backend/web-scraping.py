2b3d5e5217701e1b6f6f4b57fc635a99859f74c2
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2023-01-21 17:30:14 -0800

    Made it efficient

--- backend/web-scraping.py
+++ backend/web-scraping.py
@@ -16,42 +16,35 @@ def amazonCollector(name, db, random_IP):
     taga = soup.find_all("a", href=True)
 
     
-    test_string = "&s=grocery&sr=1-"
+    
     i = 1
 
     for raw_link in taga:
         if i != 4:
             link = raw_link['href']
 
-            if  test_string + str(i) in link:
-                
-                search = "https://www.amazon.ca" + link
-                page = requests.get(search, headers=HEADERS)
-
-                soup = BeautifulSoup(page.content, "lxml")
-                
             
-                title = soup.find("span", attrs={"id": 'productTitle'})
-            
-                price_whole = str(soup.find("span", attrs={"class": 'a-price-whole'}))[28:30]
-                price_decimal = str(soup.find("span", attrs={"class": 'a-price-fraction'}))[31:33]
+        
+            title = soup.find("span", attrs={"class": 'a-size-base-plus a-color-base a-text-normal'})
+        
+            price_whole = str(soup.find("span", attrs={"class": 'a-price-whole'}))[28:30]
+            price_decimal = str(soup.find("span", attrs={"class": 'a-price-fraction'}))[31:33]
 
-                if price_whole[1] == '<':
-                    price_whole = price_whole[0]
-                if price_decimal[1] == '<':
-                    price_decimal = price_decimal[0]
+            if price_whole[1] == '<':
+                price_whole = price_whole[0]
+            if price_decimal[1] == '<':
+                price_decimal = price_decimal[0]
 
-                price = float(price_whole + "." + price_decimal)
+            price = float(price_whole + "." + price_decimal)
 
-                title_value = title.string
+            title_value = title.string
 
-                db[i - 1] = {title_value, price}
+            db[i - 1] = {title_value, price}
 
-                i += 1
+            i += 1
         else:
             return db
         
-            
 
     
 
@@ -61,7 +54,7 @@ ran_list  = ["228.39.248.80", "116.132.147.250", "178.172.154.68", "139.64.230.1
 
 random_IP = random.choice(ran_list)
 
-db = amazonCollector("milk", db, random_IP)
+db = amazonCollector("water", db, random_IP)
 
 
 print(db)
