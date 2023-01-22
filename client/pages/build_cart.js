ba3c21d04b723c6f8c6c132d37b348697884f675
Shi Ning Le <shining@ualberta.ca>
2023-01-22 10:34:40 -0800

    commit

--- client/pages/build_cart.js
+++ client/pages/build_cart.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState, useEffect, useContext } from 'react';
 import { ItemsContext } from '../context/items-context.js';
 import axios from 'axios';
@@ -22,6 +23,26 @@ const BuildCart = () => {
                 },
                 items: Object.keys(items),
             });
+=======
+import { useState, useEffect, useContext } from "react";
+import { ItemsContext } from "../context/items-context.js";
+import axios from "axios";
+import Layout from "../components/Layout/Layout";
+import styles from "../styles/build_cart.module.css";
+import Button from "../components/Button/Button";
+import Card2 from "../components/Card2/Card2";
+
+const BuildCart = () => {
+	const { items, setItems } = useContext(ItemsContext);
+	const [itemsToCompare, setItemsToCompare] = useState({});
+	const [cartItems, setCartItems] = useState([]);
+	// On items change, store it is local storage
+	useEffect(() => {
+		const getItemsData = async () => {
+			const response = await axios.post("http://localhost:8000/api/items", {
+				items: Object.keys(items),
+			});
+>>>>>>> 9f1b0b6d8ecd4e3d739fbd75e4258a9b9b5bfe62
 
             Object.keys(items).forEach((key) => {
                 setItemsToCompare((prevVals) => ({
@@ -36,6 +57,7 @@ const BuildCart = () => {
         getItemsData();
     }, []);
 
+<<<<<<< HEAD
     return (
         <Layout>
             <div className={styles.container}>
@@ -102,6 +124,73 @@ const BuildCart = () => {
             </div>
         </Layout>
     );
+=======
+	function handleAddProduct(data) {
+		setCartItems((prev) => [...prev, data]);
+	}
+
+	return (
+		<Layout>
+			<div className={styles.container}>
+				<h2 className={styles.step}>Step 2: Build your cart</h2>
+				<section>
+					<div className={styles.content}>
+						<div className={styles.cards}>
+							<h3>Compare</h3>
+							{Object.keys(itemsToCompare)?.map((key) => {
+								const itemsByWebsite = itemsToCompare[key];
+								console.log(key);
+								return (
+									<>
+										<Card2
+											name={key}
+											key={itemsByWebsite.amazon.link}
+											imageSearch={items[key].imageSearch}
+											mass={items[key].mass}
+											price={itemsByWebsite.amazon.price}
+											website={itemsByWebsite.amazon.link}
+											buttonHandler={handleAddProduct}
+										></Card2>
+										<Card2
+											name={key}
+											key={itemsByWebsite.saveonfood?.link}
+											imageSearch={items[key].imageSearch}
+											mass={items[key].mass}
+											price={itemsByWebsite.saveonfood?.price}
+											website={itemsByWebsite.saveonfood?.link}
+											buttonHandler={handleAddProduct}
+										></Card2>
+									</>
+								);
+							})}
+						</div>
+					</div>
+					<div className={styles.content}>
+						<div className={styles.cards}>
+							<h3>Cart</h3>
+							{cartItems.map((item) => {
+								return (
+									<Card2
+										imageSearch={item.imageSearch}
+										name={item.name}
+										mass={item.mass}
+										price={item.price}
+										website={item.website}
+										noAdd={true}
+									/>
+								);
+							})}
+						</div>
+					</div>
+				</section>
+
+				<div className={styles.buttons}>
+					<Button text="Go Back" path="/choose" />
+				</div>
+			</div>
+		</Layout>
+	);
+>>>>>>> 9f1b0b6d8ecd4e3d739fbd75e4258a9b9b5bfe62
 };
 
 export default BuildCart;
